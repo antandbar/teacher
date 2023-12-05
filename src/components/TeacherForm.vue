1582fda88c05c741e25b8076fb483ae136b778cd
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2023-12-05 16:46:31 +0100

    feat: Se implementan campos

--- src/components/TeacherForm.vue
+++ src/components/TeacherForm.vue
@@ -1,10 +1,42 @@
 <template>
-  <div></div>
+  <section>
+    <h3>Añadir Profesor</h3>
+    <div><label>Nombre: </label><input tabindex="text" v-model="teacher.teacherName"/></div>
+    <div><label>Apellidos: </label><input tabindex="text" v-model="teacher.surname"/></div>
+    <div><label>DNI / NIF: </label><input tabindex="text" v-model="teacher.dni"/></div>
+    <div><label>Materias: </label><input tabindex="text" v-model="subject"/><button>Agregar</button></div>
+    <input type="checkbox" v-model="teacher.doc"> Documentación entregada
+    
+    <button>Agregar</button>
+  </section>
+  <section>
+    <h3>Listado de profesores</h3>
+  </section>
 </template>
 
-<sript lang="ts" setup>
+<script lang="ts" setup>
+    import { Ref, ref } from 'vue'
 
-</sript>
+    interface ITeacher {
+        teacherName: string,
+        surname: string,
+        dni: string,
+        subjects: Array<string>,
+        doc: boolean
+    }
+
+    let teacher:Ref<ITeacher> = ref({
+        teacherName: '',
+        surname: '',
+        dni: '',
+        subjects: [],
+        doc: false
+    })
+
+    let teachers:Ref<Array<ITeacher>> = ref([])
+
+    let subject: Ref<string> = ref('')
+</script>
 
 <style scoped>
 </style>
